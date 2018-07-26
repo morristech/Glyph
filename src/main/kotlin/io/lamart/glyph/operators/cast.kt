22af08cd9bd28e1a3bc24872bbf742d8735e0ac3
Danny Lamarti <lamarti.danny@gmail.com>
2018-07-26 20:24:00 +0200

    small improvement

--- src/main/kotlin/io/lamart/glyph/operators/cast.kt
+++ src/main/kotlin/io/lamart/glyph/operators/cast.kt
@@ -3,18 +3,18 @@ package io.lamart.glyph.operators
 import io.lamart.glyph.Glyph
 import io.lamart.glyph.OptionalGlyph
 
-@Suppress("UNCHECKED_CAST")
 class CastGlyph<T, R : T>(private val glyph: Glyph<T>) : Glyph<R> {
 
+    @Suppress("UNCHECKED_CAST")
     override fun get(): R = glyph.get() as R
 
     override fun set(state: R) = glyph.set(state)
 
 }
 
-@Suppress("UNCHECKED_CAST")
 class CastOptionalGlyph<T, R : T>(private val glyph: OptionalGlyph<T>) : OptionalGlyph<R> {
 
+    @Suppress("UNCHECKED_CAST")
     override fun get(): R? = glyph.get() as R
 
     override fun set(state: R) = glyph.set(state)
