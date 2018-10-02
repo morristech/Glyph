cd1d072feebd77589a40119d6aa62a7c66badc82
Danny <danny.lamarti@elements.nl>
2018-10-02 08:34:13 +0200

    Adds copyright

--- src/test/kotlin/io/lamart/glyph/composition/StateTests.kt
+++ src/test/kotlin/io/lamart/glyph/composition/StateTests.kt
@@ -1,9 +1,18 @@
+/*
+ * Copyright (c) 2018 Danny Lamarti.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.lamart.glyph.composition
 
 import io.lamart.glyph.composition.door.Door
 import io.lamart.glyph.composition.door.Location
 import io.lamart.glyph.composition.user.User
-import io.lamart.glyph.toGlyph
 import org.junit.Test
 
 class StateTests {
