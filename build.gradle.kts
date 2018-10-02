ee1ed2ca059db84cc1d7ef26615b13fb063d959d
Danny <danny.lamarti@elements.nl>
2018-10-02 08:33:46 +0200

    Adds uploading stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,26 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.70")
     testImplementation("junit:junit:4.12")
 }
+
+ext {
+    set("bintrayRepo", "maven")
+    set("bintrayName", "glyph")
+
+    set("publishedGroupId", "io.lamart.krate")
+    set("libraryName", "Glyph")
+    set("artifact", "glyph")
+    set("libraryDescription", "State management made easy.")
+
+    set("siteUrl", "https://github.com/Lamartio/Glyph/")
+    set("gitUrl", "https://github.com/Lamartio/Glyph.git")
+
+    set("libraryVersion", "0.5.0")
+
+    set("developerId", "Lamartio")
+    set("developerName", "Danny Lamarti")
+    set("developerEmail", "")
+
+    set("licenseName", "MIT")
+    set("licenseUrl", "https://opensource.org/licenses/MIT")
+    set("allLicenses", arrayOf("MIT"))
+}
